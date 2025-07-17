a60c85fe4d09c391c1c1cdd413d24d13112519c3
lesia_onyshchuk <lkane443@gmail.com>
2025-07-17 14:26:38 +0300

    swiper pagination css

--- src/components/FeedbackItem/FeedbackItem.jsx
+++ src/components/FeedbackItem/FeedbackItem.jsx
@@ -2,14 +2,21 @@ import css from "./FeedbackItem.module.css";
 
 const FeedbackItem = ({ item }) => {
   return (
-    <div className={css.itemBox}>
-      <img src={item.logo} alt="" />
-      <p className={css.content}>{item.content}</p>
-      <div className={css.personBox}>
-        <img src={item.photo} about={item.name} />
-        <div className={css.person}>
-          <h2 className={css.name}>{item.name}</h2>
-          <h3 className={css.role}>{item.role}</h3>
+    <div className={css.card}>
+      <div className={css.itemBox}>
+        <img
+          src={item.logo}
+          alt="logo"
+          className={css.logo}
+          width={item.width}
+        />
+        <p className={css.content}>{item.content}</p>
+        <div className={css.personBox}>
+          <img src={item.photo} about={item.name} className={css.photo} />
+          <div className={css.person}>
+            <h2 className={css.name}>{item.name}</h2>
+            <h3 className={css.role}>{item.role}</h3>
+          </div>
         </div>
       </div>
     </div>
--- src/components/FeedbackList/FeedbackList.jsx
+++ src/components/FeedbackList/FeedbackList.jsx
@@ -1,17 +1,32 @@
 import data from "../../feedbacks.json";
 import FeedbackItem from "../FeedbackItem/FeedbackItem.jsx";
 import css from "./FeedbackList.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const FeedbackList = () => {
   return (
-    <div>
-      <ul className={css.listBox}>
+    <div className={css.sliderWrapper}>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+        }}
+      >
         {data.map((item) => (
-          <li key={item.id}>
+          <SwiperSlide key={item.id}>
             <FeedbackItem item={item} />
-          </li>
+          </SwiperSlide>
         ))}
-      </ul>
+      </Swiper>
     </div>
   );
 };
